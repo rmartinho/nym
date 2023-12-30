7df39ffa2ba0f2668e06fd3ca405af0afd9279b7
R. Martinho Fernandes <rmf@rmf.io>
2023-12-30 02:18:06 +0100

    Fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 #![warn(missing_docs)]
 #![deny(clippy::correctness)]
 
-//! An implementation of a pseudonym system as described in https://www.princeton.edu/~rblee/ELE572Papers/Fall04Readings/lrsw.pdf
+//! An implementation of a pseudonym system as described in <https://www.princeton.edu/~rblee/ELE572Papers/Fall04Readings/lrsw.pdf>
 
 pub mod error;
 pub mod key;
